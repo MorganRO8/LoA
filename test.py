fc56993181c72a42a7eec6aa237426311d922b00
MorganRO8 <47795945+MorganRO8@users.noreply.github.com>
2023-06-12 16:47:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -14,13 +14,16 @@ class TestMain(unittest.TestCase):
 
     # Test the main function
     def test_main(self):
+        print("Testing main function...")
         # Mock the argparse.ArgumentParser.parse_args function to return the automatic dictionary
         with patch('argparse.ArgumentParser.parse_args', return_value=automatic):
             # Call the main function
             main()
+        print("Main function test completed.")
 
     # Test the Scrape function
     def test_scrape(self):
+        print("Testing Scrape function...")
         # Import the Scrape function
         from Scrape import Scrape
 
@@ -34,9 +37,11 @@ class TestMain(unittest.TestCase):
 
             # Check that the Scrape function returned the correct result
             self.assertEqual(result, {'paper1': 'content1'})
+        print("Scrape function test completed.")
 
     # Test the snorkel_train function
     def test_snorkel_train(self):
+        print("Testing snorkel_train function...")
         # Import the snorkel_train function
         from snorkel_train import snorkel_train
 
@@ -50,9 +55,11 @@ class TestMain(unittest.TestCase):
 
             # Check that the snorkel_train function returned the correct result
             self.assertEqual(result, {'paper1': 'content1'})
+        print("snorkel_train function test completed.")
 
     # Test the Inference function
     def test_inference(self):
+        print("Testing Inference function...")
         # Import the Inference function
         from Inference import Inference
 
@@ -66,7 +73,22 @@ class TestMain(unittest.TestCase):
 
             # Check that the Inference function returned the correct result
             self.assertEqual(result, {'paper1': 'content1'})
+        print("Inference function test completed.")
 
 # Run the tests
 if __name__ == '__main__':
     unittest.main()
+
+# Create a test suite
+suite = unittest.TestSuite()
+
+# Add tests to the test suite
+suite.addTest(TestMain('test_main'))
+suite.addTest(TestMain('test_scrape'))
+suite.addTest(TestMain('test_snorkel_train'))
+suite.addTest(TestMain('test_inference'))
+
+# Run the test suite
+runner = unittest.TextTestRunner()
+print("Running tests...")
+runner.run(suite)
